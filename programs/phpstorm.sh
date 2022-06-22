0e425f81d05cb53a0ab204bd447946d794ce2dfb
Paul <gp22@tutanota.com>
2022-06-21 20:39:18 -0500

    add programs

---
+++ programs/phpstorm.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# PhpStorm
+# https://www.jetbrains.com/phpstorm/download/#section=linux
+echo "Installing PhpStorm"
+flatpak install -y com.jetbrains.PhpStorm 
---
+++ programs/torbrowser.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Tor browser launcher
+echo "Installing Tor Browser Launcher"
+flatpak install flathub -y com.github.micahflee.torbrowser-launcher
