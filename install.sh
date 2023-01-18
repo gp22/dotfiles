5aaee03713aa861084ae8561d5f4fa45ee40a287
Paul Garcia <gp22@users.noreply.github.com>
2023-01-17 20:29:30 -0600

    add install power level 10k zsh theme

--- install.sh
+++ install.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+# Install power level 10k zsh theme
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
 # Create symlinks
 ./symlink.sh
