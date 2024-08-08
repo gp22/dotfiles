bd79d5aa5ec92ad9838ecf89a3ff125f47a39426
Paul Garcia <gp22@users.noreply.github.com>
2024-08-08 17:28:15 -0600

    Add nightfox to nvim

---
+++ nvim/lua/custom/plugins/nightfox.lua
@@ -0,0 +1,20 @@
+return {
+  {
+    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+    'EdenEast/nightfox.nvim',
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme.
+    priority = 1000, -- Make sure to load this before all the other start plugins.
+    init = function()
+      require('nightfox').setup {
+        options = {
+          styles = {
+            comments = 'italic',
+            keywords = 'bold',
+            types = 'italic,bold',
+          },
+        },
+      }
+      vim.cmd.colorscheme 'nordfox'
+    end,
+  },
+}
