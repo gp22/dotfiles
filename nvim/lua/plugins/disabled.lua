12339e739801f386b6dfa34d758cb460eaea61d3
Paul Garcia <gp22@users.noreply.github.com>
2024-10-06 10:27:39 -0600

    Disable catppuccin

--- nvim/lua/plugins/disabled.lua
+++ nvim/lua/plugins/disabled.lua
@@ -2,5 +2,5 @@ return {
   { "echasnovski/mini.ai", enabled = false },
   { "ellisonleao/gruvbox.nvim", enabled = false },
   { "folke/tokyonight.nvim", enabled = false },
-  --{ "catppuccin/nvim", enabled = false },
+  { "catppuccin/nvim", enabled = false },
 }
