a110bd75f54f919cb172e7c47bd95d4feada8ccf
gp22 <gp22@users.noreply.github.com>
2024-10-23 10:49:25 -0600

    Add bufdelete, re-enable bufferline

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -66,8 +66,8 @@ map('n', '[b', '<cmd>bprevious<cr>', { desc = 'Prev Buffer' })
 map('n', ']b', '<cmd>bnext<cr>', { desc = 'Next Buffer' })
 map('n', '<leader>bb', '<cmd>e #<cr>', { desc = 'Switch to Other Buffer' })
 map('n', '<leader>`', '<cmd>e #<cr>', { desc = 'Switch to Other Buffer' })
---map('n', '<leader>bd', '<cmd>:lua MiniBufremove.delete()<cr>', { desc = 'Delete Buffer' })
-map('n', '<leader>bD', '<cmd>:bd<cr>', { desc = 'Delete Buffer and Window' })
+map('n', '<leader>bd', '<cmd>:Bdelete<cr>', { desc = 'Delete Buffer' })
+--map('n', '<leader>bD', '<cmd>:bd<cr>', { desc = 'Delete Buffer and Window' })
 
 -- Clear search with <esc>
 map({ 'i', 'n' }, '<esc>', '<cmd>noh<cr><esc>', { desc = 'Escape and Clear hlsearch' })
